fd3e69ce949302fd66ced7e2b2af64ba175e2bda
Yayou <78508960+YaYouLeKyou@users.noreply.github.com>
2021-12-18 00:59:49 +0100

    Add files via upload

---
+++ Premiers pas/events/index.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('#btn');
+const img = document.getElementById('img');
+
+btn.addEventListener('click', () => {
+    console.log('show') 
+    alert('ça a changé')
+    
+})
